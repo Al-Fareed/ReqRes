2062ef2ae002303f66bd6866854425a51f22e3f4
Al-Fareed <alfareedss472>
2026-02-06 11:15:48 +0530

    added working test case

--- src/test/java/org/app/LoginTest.java
+++ src/test/java/org/app/LoginTest.java
@@ -4,6 +4,7 @@ import io.restassured.response.Response;
 import org.app.builder.LoginRequestBuilder;
 import org.app.client.LoginApiClient;
 import org.app.models.request.LoginRequest;
+import org.app.models.response.LoginResponse;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,6 +21,8 @@ public class LoginTest {
                 );
 
         Response response = LoginApiClient.login(request);
+        LoginResponse loginResponse = response.as(LoginResponse.class);
+        System.out.println(loginResponse.getToken());
 
         assertThat(response.getStatusCode()).isEqualTo(200);
         assertThat(response.jsonPath().getString("token")).isNotBlank();
